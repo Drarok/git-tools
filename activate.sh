50b58c870177735a189b558ccc74b7c1818e06e1
Mat Gadd <source@catchall.drarok.com>
2024-04-03 14:59:52 +0100

    Update documentation to match current behaviour, add missing files

---
+++ activate.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+ROOT_DIR=$(dirname $_)
+
+if [[ "$PATH" == *"$ROOT_DIR"* ]]; then
+  echo "[WARN] Path already includes git-tools, skipped"
+else
+  export PATH="$PATH:$ROOT_DIR"
+fi
+
+function _git_tools_alias_path {
+  CANDIDATES=("aliases.sh" "aliases-default.sh")
+  for CANDIDATE_NAME in ${CANDIDATES[@]}; do
+    CANDIDATE_PATH="$ROOT_DIR/$CANDIDATE_NAME"
+    if [ -f "$CANDIDATE_PATH" ]; then
+      echo $CANDIDATE_PATH
+      return
+    fi
+  done
+}
+
+function _git_tools_load {
+  ALIAS_PATH=$(_git_tools_alias_path)
+  if [ ! -z "$ALIAS_PATH" ]; then
+    source "$ALIAS_PATH"
+  fi
+}
+
+_git_tools_load
